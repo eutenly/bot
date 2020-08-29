a76a543847a1f595bb63d05aa6a30765f2c70520
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-29 03:49:15 -0700

    Voice channel updates are now ignored when updating permissions cache

--- src/gateway/events/channelUpdate.ts
+++ src/gateway/events/channelUpdate.ts
@@ -2,14 +2,18 @@ import Client from "../../classes/Client/Client";
 import Guild from "../../classes/Guild/Guild";
 
 interface EventData {
+    type: number;
     guild_id: string;
 }
 
-export default async function roleUpdate(client: Client, data: EventData) {
+export default async function channelUpdate(client: Client, data: EventData) {
 
     // Get guild
     const guild: Guild | undefined = client.guilds.get(data.guild_id);
     if (!guild) return;
+
+    // Ignore voice channels
+    if (data.type === 2) return;
 
     // Calculate denied permissions
     guild.calculateDeniedPermissions();
