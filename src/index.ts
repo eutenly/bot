aca4b068ee1f00a5e51032c5c4af44cce4c5f576
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-29 21:48:50 -0700

    Added log for when the bot is online

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import dotenv from "dotenv";
 import Client from "./classes/Client/Client";
 
@@ -6,3 +7,10 @@ dotenv.config();
 
 // Create client
 const client: Client = new Client(process.env.BOT_TOKEN || "");
+
+// Ready
+client.on("ready", () => {
+
+    // Log
+    console.log(chalk.green("Bot online!"));
+});
