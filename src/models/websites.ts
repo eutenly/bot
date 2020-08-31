c390fa01a9498bff15b9d91030c3a4d2261461ff
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-30 21:22:47 -0700

    Switched from typegoose to ts-mongoose

--- src/models/websites.ts
+++ src/models/websites.ts
@@ -1,12 +1,14 @@
-export default class Websites {
+import { createSchema, Type } from "ts-mongoose";
+
+export const websitesSchema = createSchema({
 
     // The URL for the website
-    _id: string;
+    _id: Type.string(),
 
     // Custom fields for the embed
-    fields: [{
-        title: string,
-        description: string,
-        inline: boolean
-    }];
-};
+    fields: Type.array().of({
+        title: Type.string(),
+        description: Type.string(),
+        inline: Type.boolean()
+    })
+});
