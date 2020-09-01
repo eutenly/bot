87c99f248f7ce2144b81926e4d40d9e51678b30f
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-31 18:56:28 -0700

    Eval command now formats output

--- src/modules/eval.ts
+++ src/modules/eval.ts
@@ -1,4 +1,7 @@
+import Channel from "../classes/Channel/Channel";
+import Client from "../classes/Client/Client";
 import Embed from "../classes/Embed/Embed";
+import Guild from "../classes/Guild/Guild";
 import Message from "../classes/Message/Message";
 
 export default async function (message: Message) {
@@ -10,7 +13,68 @@ export default async function (message: Message) {
     const code = message.content.split("eval").slice(1).join("eval");
 
     // Done function
-    const done = () => message.addReaction("✅");
+    const done = (result: any) => {
+
+        // React
+        message.addReaction("✅");
+
+        // Embed
+        const embed = new Embed()
+            .setAuthor("JavaScript Evaluation", "https://cdn.auth0.com/blog/es6rundown/logo.png")
+            .setColor(0x42f44e);
+
+        // Define formatted result
+        let formattedResult: string | undefined;
+
+        // Client
+        if (result instanceof Client) embed
+            .setTitle("Client")
+            .setDescription(`User ID: ${result.id}`)
+            .addField("Sequence Number", result.sequence?.toLocaleString(), true)
+            .addField("Guilds", result.guilds.size.toLocaleString(), true)
+            .addField("Cached Channels", result.channels.size.toLocaleString(), true);
+
+        // Messages
+        else if (result instanceof Message) embed
+            .setTitle("Message")
+            .setDescription(`\`\`\`\n${result.content}\`\`\``)
+            .addField("Guild", result.guild ? `${result.guild.name} (${result.guild.id})` : "DMs", true)
+            .addField("Author", `<@${result.authorID}> (${result.authorID})`, true)
+            .addField("Channel", `<#${result.channel.id}> (${result.channel.id})`, true);
+
+        // Channels
+        else if (result instanceof Channel) embed
+            .setTitle("Channel")
+            .setDescription(`<#${result.id}> (${result.id})`)
+            .addField("Guild", result.guild ? `${result.guild.name} (${result.guild.id})` : "DMs", true)
+            .addField("Cached Messages", result.messages.size.toLocaleString(), true);
+
+        // Guilds
+        else if (result instanceof Guild) embed
+            .setTitle("Guild")
+            .setDescription(`${result.name} (${result.id})`)
+            .addField("Denied Permissions", result.deniedPermissions.size > 10 ? `${result.deniedPermissions.size} Channels` : [...result.deniedPermissions.keys()].map((channelID: string) => `- <#${channelID}> (${channelID})`).join("\n"));
+
+        // Objects and arrays
+        else if (typeof result === "object") {
+
+            try {
+                formattedResult = JSON.stringify(result, null, 4);
+            }
+            catch (e) {
+                formattedResult = `[object ${result.constructor.name}]`;
+            }
+        }
+
+        // Strings, numbers, bools, and null
+        else if (result !== undefined) formattedResult = result;
+
+        // Result
+        if (formattedResult) embed.setDescription(`\`\`\`\n${formattedResult}\`\`\``);
+
+        // Send
+        message.channel.sendMessage(embed);
+    };
 
     // Error function
     const err = (e: Error) => {
@@ -18,7 +82,7 @@ export default async function (message: Message) {
         // React
         message.addReaction("❌").catch(() => { });
 
-        // Send
+        // Embed
         const embed = new Embed()
             .setTitle("Eval Error")
             .setAuthor("JavaScript Evaluation", "https://cdn.auth0.com/blog/es6rundown/logo.png")
