3d7c2bb4e55252d9275fc583540ed3771818ad5a
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-31 19:05:01 -0700

    Removed example command

--- src/modules/commandRouter/routes.ts
+++ src/modules/commandRouter/routes.ts
@@ -1,22 +1,14 @@
 import Message from "../../classes/Message/Message";
 import evalCommand from "../eval";
-import exampleCommand from "../example";
 
 // Define routes
 export const routes: Command[] = [
-    {
-        name: "Example",
-        information: "Example command",
-        inputs: ["example"],
-        module: exampleCommand,
-        allowParams: true
-    },
     {
         name: "Eval",
         information: "Owner JavaScript Eval",
         inputs: ["eval"],
         module: evalCommand,
-        allowParams: true,
+        allowParams: true
     }
 ];
 
--- src/modules/example.ts
+++
@@ -1,6 +0,0 @@
-import Message from "../classes/Message/Message";
-
-export default async function Example(message: Message) {
-
-    console.log(message.content);
-}
