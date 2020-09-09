b1a03df560e7d739ef487db97e7fc7bd33dcd127
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-09-08 18:43:01 -0700

    Added support for viewing twitter users

---
+++ src/modules/twitter/user/main.ts
@@ -0,0 +1,32 @@
+import Command from "../../../classes/Command/Command";
+import Embed from "../../../classes/Embed/Embed";
+import Message from "../../../classes/Message/Message";
+import getAuthorizationHeader from "../getAuthorizationHeader";
+import embed from "./embed";
+import parse from "./parse";
+import view from "./view";
+
+export default async function main(message: Message, user: string, type: string, commandHistoryIndex?: number) {
+
+    // Create command
+    const command: Command = new Command(message.client, {
+        name: "twitterUser",
+        message,
+        getURL: (): string => `https://api.twitter.com/1.1/users/show.json?${type === "id" ? "user_id" : "screen_name"}=${encodeURIComponent(user)}`,
+        connectionName: "twitter",
+        getAuthorizationHeader,
+        parser: parse,
+        getEmbed: embed,
+        view
+    }, (m: Message, chIndex: number) => main(m, user, type, chIndex), commandHistoryIndex);
+    await command.uninitializedConnection;
+
+    // Fetch
+    await command.fetch();
+
+    // Get embed
+    const commandEmbed: Embed = command.getEmbed(command, command.data);
+
+    // Send
+    command.send(commandEmbed);
+}
---
+++ src/modules/twitter/user/parse.ts
@@ -0,0 +1,36 @@
+export interface TwitterUser {
+    id: string;
+    name: string;
+    handle: string;
+    bio: string;
+    followers: number;
+    following: number;
+    tweets: number;
+    likes: number;
+    location?: string;
+    url?: string;
+    banner?: string;
+    createdOn: string;
+}
+
+export default function parse(data: any): TwitterUser | undefined {
+
+    // No user
+    if (data.errors) return;
+
+    // Return
+    return {
+        id: data.id_str,
+        name: data.name,
+        handle: data.screen_name,
+        bio: data.description,
+        followers: data.followers_count,
+        following: data.friends_count,
+        tweets: data.statuses_count,
+        likes: data.favourites_count,
+        location: data.location,
+        url: data.url && data.entities.url.urls[0].expanded_url,
+        banner: data.profile_banner_url,
+        createdOn: data.created_at
+    };
+}
