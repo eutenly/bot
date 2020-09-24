24f07c55cfb8fc05096401e5f6ae7283c0601516
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-09-24 13:16:18 -0700

    Fixed consistency and auto formatting

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -1,24 +1,25 @@
 import Embed from "../classes/Embed/Embed";
 import Message from "../classes/Message/Message";
-import { routes } from "./commandRouter/routes";
+import { routes, CommandRoute } from "./commandRouter/routes";
 
 export default async function (message: Message) {
+
     // Split for arguments
-    const args = message.content.split(" ");
+    const args: string[] = message.content.split(" ");
 
     // Get prefix
-    const activePrevix = message.guild?.prefix || process.env.DEFAULT_PREFIX;
+    const activePrevix: string = message.guild?.prefix || process.env.DEFAULT_PREFIX || "";
 
     // Embed
-    const embed = new Embed()
-    .setTitle("Eutenly Help Center")
-    .setColor(0x820020);
+    const embed: Embed = new Embed()
+        .setTitle("Eutenly Help Center")
+        .setColor(0x820020);
 
     // Switch for amount of arguments
     switch (args.length) {
         case 1:
             // When there are no arguments, show the help embed. For each route, make a field.
-            routes.forEach(function (route) {
+            routes.forEach((route: CommandRoute) => {
                 // Ignore private commands
                 if (route.private) return;
 
@@ -32,7 +33,7 @@ export default async function (message: Message) {
             return;
         case 2:
             // When there are arguments, search for the command
-            const route = routes.find((route) => route.name.toLowerCase() === args[1].toLowerCase());
+            const route = routes.find((route: CommandRoute) => route.name.toLowerCase() === args[1].toLowerCase());
 
             // If no command (or command private), show error
             if (!route || route.private) {
@@ -43,8 +44,8 @@ export default async function (message: Message) {
 
             // If command present, display information
             embed.setDescription("Information about `" + route.name + "`")
-            .addField("Description", route.information)
-            .addField("Usage", route.inputs.join(", "));
+                .addField("Description", route.information)
+                .addField("Usage", route.inputs.join(", "));
             await message.channel.sendMessage(embed);
             return;
     }
