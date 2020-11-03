f373e36406133421c39727ec624d825c57189821
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-03 11:44:05 -0800

    Added searching for saved links

--- src/modules/savedLinks/main.ts
+++ src/modules/savedLinks/main.ts
@@ -1,5 +1,6 @@
 import Command from "../../classes/Command/Command";
 import Message from "../../classes/Message/Message";
+import { SavedLink } from "../../models/users";
 import embed from "./embed";
 import parse from "./parse";
 import view from "./view";
@@ -7,11 +8,27 @@ import view from "./view";
 export default async function main(message: Message, commandHistoryIndex?: number): Promise<Command | undefined> {
 
     // Get params
-    let page: number = parseInt(message.commandContent.split(" ").slice(1).join(" ")) || 1;
-    if (page < 1) page = 1;
+    const PARAMS: string[] = message.commandContent.split(" ").slice(1);
+    let query: string | undefined = PARAMS[0]?.toLowerCase();
+    let pageInput: string | undefined = PARAMS[1];
+
+    // Swap parameters
+    if ((!parseInt(pageInput)) && (parseInt(query))) {
+        query = PARAMS[1]?.toLowerCase();
+        pageInput = PARAMS[0];
+    }
+
+    // Parse page
+    const page: number = parseInt(pageInput) || 1;
 
     // Get user data
     const userData = await message.author.getData();
+
+    // Get saved links
+    let savedLinks: SavedLink[] = userData?.savedLinks || [];
+
+    // Filter saved links
+    savedLinks = savedLinks.filter((l: SavedLink) => l.url.includes(query || ""));
 
     // Create command
     const command: Command = new Command(message.client, {
@@ -19,7 +36,7 @@ export default async function main(message: Message, commandHistoryIndex?: numbe
         type: "savedLinks",
         message,
         input: "links",
-        allData: userData?.savedLinks,
+        allData: savedLinks,
         splitPages: 5,
         parser: parse,
         getEmbed: embed,
