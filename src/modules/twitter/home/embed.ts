7d266954569d2e288db856aececf6a82787b5c37
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-03 13:59:09 -0800

    Fixed bug with twitter home compact embed not working

--- src/modules/twitter/home/embed.ts
+++ src/modules/twitter/home/embed.ts
@@ -10,11 +10,15 @@ export default function embed(command: Command, data?: TwitterHome): Embed {
 
     // Embed
     if (!data) return new Embed();
+
+    const tweets: TwitterTweet[] = command.compactMode ? data.tweets.slice(0, 3) : data.tweets;
+    const timeline: TwitterTweet[] = command.compactMode ? data.timeline.slice(0, 3) : data.timeline;
+
     const embed = new Embed()
         .setAuthor("Twitter", "https://1000logos.net/wp-content/uploads/2017/06/Twitter-Logo.png")
         .setColor(0x1da1f2)
-        .addField("Your Tweets", `${data.tweets.map((t: TwitterTweet, i: number) => `**t-${i + 1}. [${t.user.name} (@${t.user.handle})](https://twitter.com/${t.user.handle}/status/${t.id})**: ${truncateString(t.text.replace(/\n/g, " "), 50)}`).join("\n")}\n\n\u2022 Use \`${prefix}view <Tweet Number>\` to view a Tweet\n\u2022 Use \`${prefix}view tweets\` to view more of your Tweets\n\u200b`)
-        .addField("Your Timeline", `${data.timeline.map((t: TwitterTweet, i: number) => `**tl-${i + 1}. [${t.user.name} (@${t.user.handle})](https://twitter.com/${t.user.handle}/status/${t.id})**: ${truncateString(t.text.replace(/\n/g, " "), 50)}`).join("\n")}\n\n\u2022 Use \`${prefix}view <Tweet Number>\` to view a Tweet\n\u2022 Use \`${prefix}twitter timeline\` to view your timeline\n\u200b`)
+        .addField("Your Tweets", `${tweets.map((t: TwitterTweet, i: number) => `**t-${i + 1}. [${t.user.name} (@${t.user.handle})](https://twitter.com/${t.user.handle}/status/${t.id})**: ${truncateString(t.text.replace(/\n/g, " "), 50)}`).join("\n")}\n\n\u2022 Use \`${prefix}view <Tweet Number>\` to view a Tweet\n\u2022 Use \`${prefix}view tweets\` to view more of your Tweets\n\u200b`)
+        .addField("Your Timeline", `${timeline.map((t: TwitterTweet, i: number) => `**tl-${i + 1}. [${t.user.name} (@${t.user.handle})](https://twitter.com/${t.user.handle}/status/${t.id})**: ${truncateString(t.text.replace(/\n/g, " "), 50)}`).join("\n")}\n\n\u2022 Use \`${prefix}view <Tweet Number>\` to view a Tweet\n\u2022 Use \`${prefix}twitter timeline\` to view your timeline\n\u200b`)
         .setBranding();
 
     // Return
