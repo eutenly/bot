80e712477ae52345b7b9fc5d236b7d6919996dff
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-06 01:24:47 -0800

    Modularized `reactionAdded` module

--- src/modules/reactionAdded.ts
+++ src/modules/reactionAdded.ts
@@ -1,6 +1,7 @@
 import Command from "../classes/Command/Command";
-import Embed from "../classes/Embed/Embed";
 import Reaction from "../classes/Reaction/Reaction";
+import setCompactMode from "./reactions/setCompactMode";
+import setPage from "./reactions/setPage";
 
 export default async function reactionAdded(reaction: Reaction) {
 
@@ -15,54 +16,14 @@ export default async function reactionAdded(reaction: Reaction) {
     if (!reaction.user.checkCooldown()) return;
 
     // Previous page
-    if ((reaction.id === reaction.client.eutenlyEmojis.get("left_arrow")) && (command.searchManager) && (command.searchManager.page)) {
-
-        // Set page
-        command.searchManager.setPage(command.searchManager.page - 1);
-
-        // Remove reaction
-        if (reaction.guild) reaction.remove();
-    }
+    if ((reaction.id === reaction.client.eutenlyEmojis.get("left_arrow")) && (command.searchManager)) setPage(reaction, command, -1);
 
     // Next page
-    else if ((reaction.id === reaction.client.eutenlyEmojis.get("right_arrow")) && (command.searchManager) && (command.searchManager.page)) {
-
-        // Set page
-        command.searchManager.setPage(command.searchManager.page + 1);
-
-        // Remove reaction
-        if (reaction.guild) reaction.remove();
-    }
+    else if ((reaction.id === reaction.client.eutenlyEmojis.get("right_arrow")) && (command.searchManager)) setPage(reaction, command, 1);
 
     // Compact
-    else if ((reaction.id === reaction.client.eutenlyEmojis.get("compact")) && (!command.compactMode)) {
-
-        // Set cooldown
-        command.message.author.setCooldown(2000);
-
-        // Set compact mode
-        command.compactMode = true;
-
-        // Get embed
-        const embed: Embed = command.getEmbed(command, command.searchManager ? (command.searchManager.cache.get(command.searchManager.page as number) || []) : command.data);
-
-        // Send
-        command.send(embed);
-    }
+    else if ((reaction.id === reaction.client.eutenlyEmojis.get("compact")) && (!command.compactMode)) setCompactMode(command, true);
 
     // Expand
-    else if ((reaction.id === reaction.client.eutenlyEmojis.get("expand")) && (command.compactMode)) {
-
-        // Set cooldown
-        command.message.author.setCooldown(2000);
-
-        // Set compact mode
-        command.compactMode = false;
-
-        // Get embed
-        const embed: Embed = command.getEmbed(command, command.searchManager ? (command.searchManager.cache.get(command.searchManager.page as number) || []) : command.data);
-
-        // Send
-        command.send(embed);
-    }
+    else if ((reaction.id === reaction.client.eutenlyEmojis.get("expand")) && (command.compactMode)) setCompactMode(command, false);
 }
