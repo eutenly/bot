49e6602972a07eb126df4321ef1773150eb0e036
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-08 00:23:04 -0800

    `reactionRemoved` now gets user object

--- src/modules/reactionRemoved.ts
+++ src/modules/reactionRemoved.ts
@@ -1,6 +1,7 @@
 import Command from "../classes/Command/Command";
 import Embed from "../classes/Embed/Embed";
 import PartialReaction from "../classes/PartialReaction/PartialReaction";
+import User from "../classes/User/User";
 
 export default async function reactionRemoved(partialReaction: PartialReaction) {
 
@@ -11,8 +12,14 @@ export default async function reactionRemoved(partialReaction: PartialReaction) 
     // Restrict to command author
     if (partialReaction.userID !== command.message.author.id) return;
 
+    // Get user
+    const user: User = partialReaction.client.users.get(partialReaction.userID) || new User(partialReaction.client, {
+        id: partialReaction.userID,
+        bot: false
+    });
+
     // Cooldown not done
-    if (!partialReaction.client.users.get(partialReaction.userID)?.checkCooldown()) return;
+    if (!user.checkCooldown()) return;
 
     // Compact
     if ((partialReaction.id === partialReaction.client.eutenlyEmojis.get("compact")) && (command.compactMode)) {
