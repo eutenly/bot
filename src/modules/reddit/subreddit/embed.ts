9483f54955c793b61f4ad491c4581ab300c25cbb
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-21 13:52:09 -0800

    Replaced some dashes with `|`'s for reddit post lists

--- src/modules/reddit/subreddit/embed.ts
+++ src/modules/reddit/subreddit/embed.ts
@@ -35,7 +35,7 @@ export default function embed(command: Command, data?: Subreddit): Embed {
         .addField("Subscribers", data.subscribers.toLocaleString(), true)
         .addField("Online Users", data.onlineUsers.toLocaleString(), true);
 
-    embed.addField("Hot Posts", `${data.hotPosts.map((p: BasicPost, i: number) => `**p-${i + 1}. [${truncateString(p.title, 35)}](https://reddit.com/r/${data.name}/comments/${p.id})** - ${p.score.toLocaleString()} Point${p.score === 1 ? "" : "s"}`).join("\n")}\n\n\u2022 Use \`${prefix}view <Post Number>\` to view a post\n\u2022 Use \`${prefix}view posts\` to view more posts`);
+    embed.addField("Hot Posts", `${data.hotPosts.map((p: BasicPost, i: number) => `**p-${i + 1}. [${truncateString(p.title, 35)}](https://reddit.com/r/${data.name}/comments/${p.id})** | ${p.score.toLocaleString()} Point${p.score === 1 ? "" : "s"}`).join("\n")}\n\n\u2022 Use \`${prefix}view <Post Number>\` to view a post\n\u2022 Use \`${prefix}view posts\` to view more posts`);
 
     if (!command.compactMode) embed.addField("Created", parseDate(data.createdAt));
 
